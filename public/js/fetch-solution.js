f0051225ff0f66df18eb3865c2713c6c76c961e2
Andy <andy.dot.heng@gmail.com>
2017-12-22 22:12:04 -0500

    create fetch solution

---
+++ public/js/fetch-solution.js
@@ -0,0 +1,39 @@
+//Fetch solution
+
+//Get the signed request
+function signRequest(file, cb) {
+  const url = `/sign?file_name=${file.name}&file_type=${file.type}`;
+  fetch(url)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      cb(data);
+    })
+}
+
+//Handles the uploader
+function uploader(file, signedReq, url, cb) {
+  fetch(signedReq, {
+    method: "PUT",
+    body: file
+  })
+    .then(() => {
+      cb();
+    })
+}
+
+// Checks for event on file upload
+function listenForImage() {
+  document.getElementById("img").addEventListener("change", function() {
+    const file = document.getElementById("img").files[0];
+    if (!file) return;
+    signRequest(file, function(signed) {
+      uploader(file, signed.signed_request, signed.url, function() {
+        document.getElementById("preview").src = signed.url;
+      })
+    })
+  })
+}
+
+listenForImage();
